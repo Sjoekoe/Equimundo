e25751c023797cdd6fec0c126b1384d59aeff414
Sjoekoe <jonckershans@gmail.com>
2015-04-06 16:45:18 +0200

    Do not submit with empty form

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -33,13 +33,5 @@
 	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 	<script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
     <script src="/js/all.js"></script>
-    <script>
-        $('.comments__create-form').on('keydown', function(e) {
-            if (e.keyCode == 13) {
-                e.preventDefault();
-                $(this).submit();
-            }
-        });
-    </script>
 </body>
 </html>
