10d33e4eb2e3a0ab1e5d6f1c9501d2af1e2b6739
Sjoekoe <jonckershans@gmail.com>
2015-04-07 21:42:49 +0200

    Add ajax to giving comments

--- public/js/comment-form.js
+++ public/js/comment-form.js
@@ -1,8 +1,49 @@
-(function() {
-    $('.comments__create-form').on('keydown', function(e) {
-        if (e.keyCode == 13 && $.trim($(this).find('textarea').val()) !== "" ) {
+(function () {
+    function displayComment(data) {
+        var commentHtml = createComment(data);
+        var commentEl = $(commentHtml);
+        commentEl.hide();
+        var postsList = data.form.next();
+        console.log(postsList);
+        postsList.append(commentEl);
+        commentEl.slideDown();
+    }
+
+    function createComment(data) {
+        var html = '' +
+            '<article class="comments__comment media status-media row">' +
+            '<div class="pull-left">' +
+            '</div>' +
+            '<div class="media-body">' +
+            '<h4 class="media-heading">' + data.username+ '</h4>' +
+            data.body +
+            '</div>' +
+            '</article>';
+
+        return html;
+    }
+
+    $('.comments__create-form').on('keydown', function (e) {
+        if (e.keyCode == 13 && $.trim($(this).find('textarea').val().length !== "")) {
             e.preventDefault();
-            $(this).submit();
+
+            var form = $(this);
+            var method = form.find('input[name=_method]').val() || 'POST';
+            var data = {
+                "body": form.find('textarea[name=body]').val(),
+                "status_id": form.find('input[name=status_id]').val(),
+                "username": form.find('input[name=username]').val(),
+                "form": $(this)
+            }
+
+            $.ajax({
+                method: method,
+                url: form.prop('action'),
+                data: form.serialize(),
+                success: function () {
+                    displayComment(data);
+                }
+            });
         }
     });
 })();
