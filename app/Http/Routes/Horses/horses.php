75add0ec5bc607595ea645afe37219c2d1c70b3b
hans_J <hans@beatswitch.com>
2015-05-03 17:27:48 +0200

    Edit horse profiles

--- app/Http/Routes/Horses/horses.php
+++ app/Http/Routes/Horses/horses.php
@@ -3,4 +3,6 @@
 Route::get('horses/index', ['as' => 'horses.index', 'uses' => 'HorseController@index', 'middleware' => 'auth']);
 Route::get('horses/create', ['as' => 'horses.create', 'uses' => 'HorseController@create', 'middelware' => 'auth']);
 Route::post('horses/create', ['as' => 'horses.create', 'uses' => 'HorseController@store'])->before('csrf');
+Route::get('horses/edit/{horseId}', ['as' => 'horses.edit', 'uses' => 'HorseController@edit', 'middleware' => 'auth']);
+Route::put('horses/edit/{horseId}', ['as' => 'horses.update', 'uses' => 'HorseController@update'])->before('csrf');
 Route::get('horses/{slug}', ['as' => 'horses.show', 'uses' => 'HorseController@show', 'middleware' => 'auth']);
