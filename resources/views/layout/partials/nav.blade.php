51a6435ca0ae4cf6297466647a37910d9234a86d
hans_J <hans@beatswitch.com>
2015-05-03 17:57:28 +0200

    Add navigation tabs

--- resources/views/layout/partials/nav.blade.php
+++ resources/views/layout/partials/nav.blade.php
@@ -4,11 +4,20 @@
     </div>
     <span class="left success"><a href="#">Left</a></span>
     <span class="right">
-        @if (Auth::guest())
-            <a href="{{ route('login') }}">Sign In</a>
-            <a href="{{ route('register') }}">Register</a>
-        @else
-            <a href="{{ route('logout') }}">Logout</a>
-        @endif
+        <div class="menu-group-right">
+            @if (Auth::guest())
+                <ul class="primary condense menu-bar">
+                    <li><a href="{{ route('login') }}">Sign In</a></li>
+                    <li><a href="{{ route('register') }}">Register</a></li>
+                </ul>
+            @else
+                <ul class="primary menu-bar">
+                    <li><a href="{{ route('logout') }}">Logout</a></li>
+                    @if (Auth::user()->isAdmin())
+                        <li><a href="#">Admin Panel</a></li>
+                    @endif
+                </ul>
+            @endif
+        </div>
     </span>
 </div>
