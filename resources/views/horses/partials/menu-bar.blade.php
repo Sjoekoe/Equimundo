3c31b919dd8a49301960978be7157e934eeb95bc
hans_J <hans@beatswitch.com>
2015-06-09 20:47:59 +0200

    Implement active state

--- resources/views/horses/partials/menu-bar.blade.php
+++ resources/views/horses/partials/menu-bar.blade.php
@@ -1,12 +1,36 @@
 <div class="row">
     <div class="col s12 teal lighten-5">
         <ul class="tabs teal lighten-5">
-            <li class="tab col s2"><a href="{{ route('horse.info', $horse->slug) }}" class="teal-text">Info</a></li>
-            <li class="tab col s2"><a href="{{ route('horses.show', $horse->slug) }}" class="teal-text">Statuses</a></li>
-            <li class="tab col s2"><a href="{{ route('follows.index', $horse->slug) }}" class="teal-text">Followers</a></li>
-            <li class="tab col s2"><a href="{{ route('horses.pictures.index', $horse->slug) }}" class="teal-text">Pictures</a></li>
-            <li class="tab col s2"><a href="{{ route('pedigree.index', $horse->slug) }}" class="teal-text">Pedigree</a></li>
-            <li class="tab col s2"><a href="{{ route('palmares.index', $horse->slug) }}" class="teal-text">Palmares</a></li>
+            <li class="tab col s2">
+                <a href="{{ route('horses.show', $horse->slug) }}" class="teal-text {{ Active::route('horses.show') }}">
+                    Statuses
+                </a>
+            </li>
+            <li class="tab col s2">
+                <a href="{{ route('horse.info', $horse->slug) }}" class="teal-text {{ Active::route('horse.info') }}">
+                    Info
+                </a>
+            </li>
+            <li class="tab col s2">
+                <a href="{{ route('follows.index', $horse->slug) }}" class="teal-text {{ Active::route('follows.index') }}">
+                    Followers
+                </a>
+            </li>
+            <li class="tab col s2">
+                <a href="{{ route('horses.pictures.index', $horse->slug) }}" class="teal-text {{ Active::route('horses.pictures.index') }}">
+                    Pictures
+                </a>
+            </li>
+            <li class="tab col s2">
+                <a href="{{ route('pedigree.index', $horse->slug) }}" class="teal-text {{ Active::route('pedigree.index') }}">
+                    Pedigree
+                </a>
+            </li>
+            <li class="tab col s2">
+                <a href="{{ route('palmares.index', $horse->slug) }}" class="teal-text {{ Active::route('palmares.index') }}">
+                    Palmares
+                </a>
+            </li>
         </ul>
     </div>
 </div>
