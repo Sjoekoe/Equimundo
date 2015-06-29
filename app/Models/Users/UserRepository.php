79cee9ebf139de0104d1936638daa893c8f8ec56
hans_J <hans@beatswitch.com>
2015-06-29 22:02:01 +0200

    Expand Search

--- app/Models/Users/UserRepository.php
+++ app/Models/Users/UserRepository.php
@@ -6,21 +6,25 @@ use HorseStories\Models\Horses\Horse;
 class UserRepository
 {
     /**
+     * @var \HorseStories\Models\Users\User
+     */
+    private $user;
+
+    /**
+     * @param \HorseStories\Models\Users\User $user
+     */
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+    /**
      * @param int $id
      * @return \HorseStories\Models\Users\User
      */
     public function findById($id)
     {
-        return User::where('id', $id)->firstOrFail();
-    }
-
-    /**
-     * @param \HorseStories\Models\Users\User $user
-     * @return array
-     */
-    public function findHorsesForSelect(User $user)
-    {
-        return Horse::with('statuses')->where('user_id', $user->id)->lists('name', 'id');
+        return $this->user->where('id', $id)->firstOrFail();
     }
 
     /**
@@ -29,7 +33,7 @@ class UserRepository
      */
     public function findConversations(User $user)
     {
-        return User::find($user->id)->conversations()->orderBy('updated_at', 'DESC')->where('deleted_at', null)->get();
+        return $this->user->find($user->id)->conversations()->orderBy('updated_at', 'DESC')->where('deleted_at', null)->get();
     }
 
     /**
@@ -37,6 +41,18 @@ class UserRepository
      */
     public function all()
     {
-        return User::all();
+        return $this->user->all();
+    }
+
+    /**
+     * @param string $input
+     * @return \HorseStories\Models\Users\User
+     */
+    public function search($input)
+    {
+        return $this->user->where('username', 'like', '%' . $input . '%')
+            ->orWhere('first_name', 'like', '%' . $input . '%')
+            ->orWhere('last_name', 'like', '%' . $input . '%')
+            ->get();
     }
 }
