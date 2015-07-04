79193ba6dd171d692793896cf709376a9ee4b119
hans_J <hans@beatswitch.com>
2015-07-04 16:52:35 +0200

    Translate to dutch

---
+++ resources/lang/nl/copy.php
@@ -0,0 +1,77 @@
+<?php
+
+return [
+    'titles' => [
+        'about_you' => 'Over Uzelf',
+        'create_horse' => 'Creëer een paard',
+        'daughters' => 'Dochters',
+        'edit' => 'Wijzig',
+        'following' => 'Volgend',
+        'horses' => 'Paarden',
+        'horses_of' => 'Paarden van',
+        'messages' => 'Berichten',
+        'results' => 'Resultaten',
+        'sons' => 'Zonen',
+        'users' => 'Gebruikers',
+        'welcome_message' => 'Welkom bij Horse Stories',
+    ],
+
+    'p' => [
+        'born' => 'Geboren:',
+        'category' => 'Categorie:',
+        'date' => 'Datum:',
+        'discipline' => 'Discipline:',
+        'empty_conversation' => 'Geen berichten meer in deze conversatie',
+        'event' => 'Evenement:',
+        'input_problems' => 'Er hebben zich problemen voorgedaan met je invoer.',
+        'life_number' => 'Levensnummer:',
+        'no_conversations' => 'Je hebt geen conversaties',
+        'no_horses_found' => 'Geen paarden gevonden',
+        'no_palmares' => 'heeft nog geen palmares',
+        'no_statuses' => 'Er zijn nog geen statusen',
+        'no_users_found' => 'Geen gebruikers gevonden',
+        'passed_away' => 'Gestorven:',
+        'please_create_horse' => 'Gelieve eerst een paard aan te maken voor je een status kan posten',
+        'ranked' => 'Plaats',
+        'welcome_message' => 'Welkom bij het social media platform waar je het leven van je paard kan delen',
+    ],
+
+    'a' => [
+        'add_achievement' => 'Voeg prestatie toe',
+        'add_family' => 'Familie toevoegen',
+        'add_father' => 'Vader toevoegen',
+        'add_father_father' => 'Vaders vader toevoegen',
+        'add_father_mother' => 'Vaders moeder toevoegen',
+        'add_mother' => 'Moeder toevoegen',
+        'add_mother_father' => 'Moeders vader toevoegen',
+        'add_mother_mother' => 'Moeders moeder toevoegen',
+        'contact_owner' => 'Contacteer de eigenaar',
+        'create_a_horse' => 'Maak een paard aan',
+        'delete' => 'Verwijderen',
+        'edit' => 'Wijzigen',
+        'edit_profile' => 'Profiel aanpassen',
+        'follow' => 'Volg',
+        'followers' => 'Volgers',
+        'info' => 'Informatie',
+        'logout' => 'Log uit',
+        'messages' => 'Berichten',
+        'my_horses' => 'Mijn paarden',
+        'palmares' => 'Palmares',
+        'pedigree' => 'Stamboom',
+        'pictures' => 'Foto\'s',
+        'show_story' => 'Toon verhaal',
+        'sign_in' => 'Log in',
+        'sign_up' => 'Registreer',
+        'statuses' => 'Statusen',
+        'timeline' => 'Tijdlijn',
+        'unfollow' => 'Ontvolg ',
+    ],
+
+    'span' => [
+        'file' => 'Bestand',
+    ],
+
+    'strong' => [
+        'whoops' => 'Oops!'
+    ],
+];
---
+++ resources/lang/nl/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+	/*
+	|--------------------------------------------------------------------------
+	| Password Reminder Language Lines
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines are the default lines which match reasons
+	| that are given by the password broker for a password update attempt
+	| has failed, such as for an invalid token or invalid new password.
+	|
+	*/
+
+	"password" => "Wachtwoorden moeten moeten minstens 6 tekens lang zijn, en overeenstemmen met de confirmatie.",
+	"user" => "We kunnen geen gebruiker vinden met dat e-mail adres",
+	"token" => "De token om je wachtwoord te herstellen is incorrect.",
+	"sent" => "We hebben u een e-mail verzonden",
+	"reset" => "Uw wachtwoord is hersteld",
+
+];
