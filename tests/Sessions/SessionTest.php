ee73fe70963476f263fad0ee75b0105a4e71d10a
hans_J <hans@beatswitch.com>
2015-07-18 15:18:56 +0200

    Session Test

--- tests/Sessions/SessionTest.php
+++ tests/Sessions/SessionTest.php
@@ -3,9 +3,8 @@ namespace Sessions;
 
 use HorseStories\Models\Settings\Setting;
 use HorseStories\Models\Users\User;
-use TestCase;
 
-class SessionTest extends TestCase
+class SessionTest extends \TestCase
 {
     /** @test */
     public function login()
@@ -22,6 +21,24 @@ class SessionTest extends TestCase
             ->type($user->email, 'email')
             ->type('password', 'password')
             ->press('Sign In')
-            ->onPage('/');
+            ->onPage('/')
+            ->dontSee('Welcome to Horse Stories');
+    }
+
+    /** @test */
+    public function logout()
+    {
+        $user = factory(User::class)->create();
+
+        factory(Setting::class)->create([
+            'user_id' => $user->id
+        ]);
+
+        $this->actingAs($user)
+            ->withSession(['foo' => 'bar'])
+            ->visit('/')
+            ->click('Sign Out')
+            ->onPage('/')
+            ->see('Welcome to Horse Stories');
     }
 }
