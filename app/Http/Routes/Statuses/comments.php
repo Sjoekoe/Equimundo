e3bcb8b7d657dea240736ea5338070e3ed8dc850
hans_J <hans@beatswitch.com>
2015-11-15 14:49:18 +0100

    Delete comments

--- app/Http/Routes/Statuses/comments.php
+++ app/Http/Routes/Statuses/comments.php
@@ -1,3 +1,4 @@
 <?php
 
 post('statuses/{status}/comments', ['as' => 'comment.store', 'uses' => 'CommentController@store', 'middleware' => 'auth']);
+get('comments/{comment}/delete', ['as' => 'comment.delete', 'uses' => 'CommentController@delete', 'middleware' => 'auth']);
--- resources/views/statuses/partials/comment.blade.php
+++ resources/views/statuses/partials/comment.blade.php
@@ -2,5 +2,6 @@
     <div class="media-body clearfix">
         <h5 class="media-heading pull-left">{{ $comment->poster()->fullName() }}</h5>
         <p>{{ $comment->body() }}</p>
+        <a href="{{ route('comment.delete', $comment->id()) }}" class="pul-right">Delete</a>
     </div>
 </article>
