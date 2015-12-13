261228cbb324b6b5322017e7ea012c5a966a71b2
hans_J <hans@beatswitch.com>
2015-12-13 18:22:51 +0100

    Another broken test

--- app/Models/Pedigrees/PedigreeConnector.php
+++ app/Models/Pedigrees/PedigreeConnector.php
@@ -52,9 +52,9 @@ class PedigreeConnector
     public function getGender($type)
     {
         if ($type == Pedigree::MOTHER || $type == Pedigree::DAUGHTER) {
-            return Horse::FEMALE;
+            return Horse::MARE;
         }
 
-        return Horse::MALE;
+        return Horse::STALLION;
     }
 }
