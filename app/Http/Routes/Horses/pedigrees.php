e3ff22db3a7801ba59d9d92fa849f6a9280c8f3f
hans_J <hans@beatswitch.com>
2015-12-19 17:45:29 +0100

    Unable to edit pedigrees

--- app/Http/Routes/Horses/pedigrees.php
+++ app/Http/Routes/Horses/pedigrees.php
@@ -2,7 +2,5 @@
 
 get('horses/{horse_slug}/pedigree', ['as' => 'pedigree.index', 'uses' => 'PedigreeController@index', 'middleware' => 'auth']);
 get('horses/{horse_slug}/pedigree/create', ['as' => 'pedigree.create', 'uses' => 'PedigreeController@create', 'middleware' => 'auth']);
-post('horses/{horse_slug}/pedigree/create', ['as' => 'pedigree.store', 'uses' => 'PedigreeController@store', 'middleware' => 'auth'])->before('csrf');
-get('pedigree/{horse_slug}/edit', ['as' => 'pedigree.edit', 'uses' => 'PedigreeController@edit', 'middleware' => 'auth']);
-put('pedigree/{horse_slug}/edit', ['as' => 'pedigree.update', 'uses' => 'PedigreeController@update', 'middleware' => 'auth'])->before('csrf');
+post('horses/{horse_slug}/pedigree/create', ['as' => 'pedigree.store', 'uses' => 'PedigreeController@store', 'middleware' => 'auth']);
 get('pedigree/{pedigree}/delete', ['as' => 'pedigree.delete', 'uses' => 'PedigreeController@delete', 'middleware' => 'auth']);
