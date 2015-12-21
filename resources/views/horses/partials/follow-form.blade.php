4d9338776b466fe3faa98fe7ec2db68cba07b2c0
hans_J <hans@beatswitch.com>
2015-12-21 17:18:10 +0100

    Update the readme file

--- resources/views/horses/partials/follow-form.blade.php
+++ resources/views/horses/partials/follow-form.blade.php
@@ -1,9 +1,9 @@
 @if (Auth::user()->isFollowing($horse))
     {{ Form::open(['route' => ['follows.destroy', $horse->id()], 'method' => 'DELETE']) }}
-        <button type="submit" class="btn btn-info">{{ trans('copy.a.unfollow') . $horse->name }}</button>
+        <button type="submit" class="btn btn-primary padding only-margin-tr">{{ trans('copy.a.unfollow') . $horse->name }}</button>
     {{ Form::close() }}
 @else
     {{ Form::open(['route' => ['follows.store', $horse->id()]]) }}
-        <button type="submit" class="btn btn-info">{{ trans('copy.a.follow') . $horse->name() }}</button>
+        <button type="submit" class="btn btn-primary padding only-margin-tr">{{ trans('copy.a.follow') . $horse->name() }}</button>
     {{ Form::close() }}
 @endif
