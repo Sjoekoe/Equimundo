855f7165ed6ef13354b31ae6ca22640127840b43
hans_J <hans@beatswitch.com>
2016-02-22 20:31:16 +0100

    Remove closure

--- app/Http/Routes/Pages/pages.php
+++ app/Http/Routes/Pages/pages.php
@@ -1,8 +1,6 @@
 <?php
 
-Route::get('/queue/receive', function() {
-    Queue::marshal();
-});
+Route::get('/queue/receive', ['as' => 'queue.receive', 'uses' => 'PagesController@queue']);
 Route::get('/tos', ['as' => 'terms_of_service', 'uses' => 'PagesController@terms']);
 Route::get('/privacy', ['as' => 'privacy', 'uses' => 'PagesController@privacy']);
 Route::get('/', ['as' => 'home', 'uses' => 'PagesController@home', 'middleware' => 'auth']);
