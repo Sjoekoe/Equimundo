9a61fc3a3a780621fdc02a27705aabc7773d82c0
hans_J <hans@beatswitch.com>
2016-03-17 20:57:11 +0100

    Add javascript to sidebar view

--- resources/views/layout/left-aside.blade.php
+++ resources/views/layout/left-aside.blade.php
@@ -18,6 +18,17 @@
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+    @if (App::environment('production'))
+        <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+            ga('create', 'UA-65362816-1', 'auto');
+            ga('send', 'pageview');
+        </script>
+    @endif
 </head>
 <body>
 <div id="container" class="effect mainnav-out aside-in aside-left aside-bright">
@@ -38,12 +49,37 @@
     @endif
 </div>
 
-
+@if (auth()->check())
+    <script>
+        var user_id = {{ auth()->user()->id() }}
+    </script>
+@endif
 
 <!-- Scripts -->
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
 <script src="/js/all.js"></script>
+<script src="/js/app.js"></script>
+@if (auth()->check() && App::environment('production'))
+    <script
+            src="//d2s6cp23z9c3gz.cloudfront.net/js/embed.widget.min.js"
+            data-domain="equimundo.besnappy.com"
+            data-lang="en"
+            data-name="{{ auth()->user()->fullName() }}"
+            data-email="{{ auth()->user()->email() }}"
+            data-background="#40c4a7"
+    >
+    </script>
+    <script>
+        window.intercomSettings = {
+            app_id: "cl2pnkcf",
+            name: "{{ auth()->user()->fullName() }}", // Full name
+            email: "{{ auth()->user()->email() }}", // Email address
+            created_at: "{{ auth()->user()->created_at }}" // Signup date as a Unix timestamp
+        };
+    </script>
+    <script>(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/cl2pnkcf';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()</script>
+@endif
 @yield('footer')
 </body>
 </html>
