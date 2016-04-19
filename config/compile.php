36fb70ac3694b0f51750eb296471db3a338e6792
Hans Jonckers <jonckershans@gmail.com>
2016-04-19 15:40:54 +0200

    Clear compiled file (#276)

--- config/compile.php
+++ config/compile.php
@@ -15,10 +15,6 @@ return [
 
 	'files' => [
 
-		realpath(__DIR__.'/../app/Providers/AppServiceProvider.php'),
-		realpath(__DIR__.'/../app/Providers/EventServiceProvider.php'),
-		realpath(__DIR__.'/../app/Providers/RouteServiceProvider.php'),
-
 	],
 
 	/*
