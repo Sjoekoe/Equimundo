e6eaf66b8065fd1e8204cc056286b5ca01df4e21
Hans Jonckers <jonckershans@gmail.com>
2016-05-09 15:12:44 +0200

    Albums

--- resources/views/albums/create.blade.php
+++ resources/views/albums/create.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     @include('layout.partials.heading')
-    <div id="page-content">
+    <div class="row">
         <div class="col-md-8 col-md-offset-2 col-sm-12">
             <div class="panel">
                 <div class="panel-heading">
