0f204f99ebfa0c7f1aabe09d55ef867447602782
Hans Jonckers <jonckershans@gmail.com>
2016-05-11 10:41:25 +0200

    Error pages

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -1,24 +1,21 @@
 @extends('layout.outer-app')
 
 @section('content')
-    <div id="container" class="cls-container">
-        <div id="bg-overlay" class="bg-img" style="background-image: url({{ asset('images/horses.jpg') }})"></div>
+    <div class="passwordBox text-center animated fadeInDown">
+        <div class="ibox-content">
+            <h1>403</h1>
+            <h3 class="font-bold">Not allowed</h3>
 
-        @include('layout.partials._outer_app_header')
+            <div class="error-desc">
+                Sorry, but You have no access to this page.
 
-        <div class="cls-content">
-            <h1 class="error-code text-mint">Not allowed</h1>
-            <p class="h4 text-thin pad-btm mar-btm">
-                <i class="fa fa-exclamation-circle fa-fw text-danger"></i>
-                You have no access to this page.
-            </p>
-            <div class="row mar-btm">
-                {{ Form::open(['route' => 'search', 'class' => 'col-xs-12 col-sm-10 col-sm-offset-1']) }}
-                {{ Form::text('search', null, ['placeholder' => trans('forms.placeholders.search') . '...', 'class' => 'form-control input-lg error-search']) }}
+                {{ Form::open(['route' => 'search', 'class' => 'form-inline m-t']) }}
+                {{ Form::text('search', null, ['placeholder' => trans('forms.placeholders.search') . '...', 'class' => 'form-control']) }}
+                <button type="submit" class="btn btn-primary">Search</button>
                 {{ Form::close() }}
+                <br>
+                <a href="{{ route('home') }}" class="btn btn-primary btn-block">Back to Homepage</a>
             </div>
-            <br>
-            <div class="pad-top"><a class="btn-link" href="{{ route('home') }}">Back to Homepage</a></div>
         </div>
     </div>
 @stop
--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -1,23 +1,21 @@
 @extends('layout.outer-app')
 
 @section('content')
-    <div id="container" class="cls-container">
-        <div id="bg-overlay" class="bg-img" style="background-image: url({{ asset('images/horses.jpg') }})"></div>
+    <div class="passwordBox text-center animated fadeInDown">
+        <div class="ibox-content">
+            <h1>500</h1>
+            <h3 class="font-bold">Oooops!</h3>
 
-        @include('layout.partials._outer_app_header')
-
-        <div class="cls-content">
-            <p class="h4 text-thin pad-btm mar-btm">
-                <i class="fa fa-exclamation-circle fa-fw text-danger"></i>
+            <div class="error-desc">
                 An error has occured. Our technical departement is working on it as we speak. Please try again later.
-            </p>
-            <div class="row mar-btm">
-                {{ Form::open(['route' => 'search', 'class' => 'col-xs-12 col-sm-10 col-sm-offset-1']) }}
-                {{ Form::text('search', null, ['placeholder' => trans('forms.placeholders.search') . '...', 'class' => 'form-control input-lg error-search']) }}
+
+                {{ Form::open(['route' => 'search', 'class' => 'form-inline m-t']) }}
+                {{ Form::text('search', null, ['placeholder' => trans('forms.placeholders.search') . '...', 'class' => 'form-control']) }}
+                <button type="submit" class="btn btn-primary">Search</button>
                 {{ Form::close() }}
+                <br>
+                <a href="{{ route('home') }}" class="btn btn-primary btn-block">Back to Homepage</a>
             </div>
-            <br>
-            <div class="pad-top"><a class="btn-link" href="{{ route('home') }}">Back to Homepage</a></div>
         </div>
     </div>
 @stop
