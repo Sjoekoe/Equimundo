4bf1a4510303428f03c0dd4e0357366b7182e14b
Hans Jonckers <jonckershans@gmail.com>
2016-05-16 09:51:37 +0200

    Remove branding (#296)

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -57,7 +57,6 @@
                 <p class="text-muted text-center"><small>Already have an account?</small></p>
                 <a class="btn btn-sm btn-white btn-block" href="{{ route('login') }}">Login</a>
             {{ Form::close() }}
-            <p class="m-t"> <small>Inspinia we app framework base on Bootstrap 3 &copy; 2014</small> </p>
         </div>
     </div>
 @endsection
