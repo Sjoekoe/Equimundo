9b101268517d5d53445c69ca8a64ca4d3e389b2a
Hans Jonckers <jonckershans@gmail.com>
2016-05-17 12:29:17 +0200

    Fix single status view (#297)

--- resources/views/statuses/show.blade.php
+++ resources/views/statuses/show.blade.php
@@ -1,12 +1,14 @@
 @extends('layout.app')
 
 @section('content')
-    <div class="col-lg-7 col-lg-offset-2 col-sm-12 mar-top">
-        <singlestatus></singlestatus>
+    <div class="row">
+        <div class="col-lg-7 col-lg-offset-2 col-sm-12 mar-top">
+            <singlestatus></singlestatus>
 
-        <template id="single-status">
-            @include('statuses.partials._status_template')
-        </template>
+            <template id="single-status">
+                @include('statuses.partials._status_template')
+            </template>
+        </div>
     </div>
 
     <script>
